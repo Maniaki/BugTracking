838f431161ddf1d5c91c465f10b6a999c8bca286
Xavier <telessiasilent@gmail.com>
2020-11-28 00:02:05 +0100

    Mise à jour classe BugTracking +Creation Entity Manager

--- src/main/java/fr/istv/Bugtracking/BugtrackingApplication.java
+++ src/main/java/fr/istv/Bugtracking/BugtrackingApplication.java
@@ -3,8 +3,21 @@ package fr.istv.Bugtracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 @SpringBootApplication
 public class BugtrackingApplication {
+
+	private static EntityManager em = null;
+
+	static {
+		try {
+			em = Persistence.createEntityManagerFactory("BugTrackingService").createEntityManager();
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BugtrackingApplication.class, args);
