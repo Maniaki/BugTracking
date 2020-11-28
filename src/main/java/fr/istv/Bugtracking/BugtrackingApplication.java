73ba7a5c21c0fe78c016466a919b3aae92cc3d7a
Maniaki <flamcourt.clement@gmail.com>
2020-11-28 10:12:32 +0100

    ajout fonctionnalité de base pour ajout/suppresion/methode recherche bug/dev

--- src/main/java/fr/istv/Bugtracking/BugtrackingApplication.java
+++ src/main/java/fr/istv/Bugtracking/BugtrackingApplication.java
@@ -3,8 +3,14 @@ package fr.istv.Bugtracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import Model.Bug;
+import Model.Dev;
+
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 @SpringBootApplication
 public class BugtrackingApplication {
@@ -18,7 +24,51 @@ public class BugtrackingApplication {
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
+	
+	public Dev createdev() {
+		Dev dev = new Dev();
+		em.persist(dev);
+		return dev;
+	}
 
+	public Dev finddev(int id) {
+		return em.find(Dev.class, id);
+	}
+	
+	public Collection<Dev> findAllDev(){
+		Query query = em.createQuery("SELECT s FROM Dev s");
+		return (Collection<Dev>) query.getResultList();
+	}
+	
+	public void removeDev(int id) {
+		Dev d = finddev(id);
+		if(d != null) {
+			em.remove(d);
+		}
+	}
+	
+	public Bug createbug() {
+		Bug b = new Bug();
+		em.persist(b);
+		return b;
+	}
+	
+	public Bug findbug(int id) {
+		return em.find(Bug.class, id);
+	}
+	
+	public Collection<Bug> findallBug(){
+		Query query = em.createQuery("SELECT l FROM Bug l");
+		return (Collection<Bug>) query.getResultList();
+	}
+	
+	public void removeBug(int id) {
+		Bug b = findbug(id);
+		if(b != null) {
+			em.remove(b);
+		}
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BugtrackingApplication.class, args);
 	}
