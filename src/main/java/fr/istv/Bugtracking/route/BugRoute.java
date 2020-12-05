04e382bd8a780550bbeaeab63216f143d73acf8d
Xavier <telessiasilent@gmail.com>
2020-12-05 09:28:08 +0100

    Up BugRoute.java et SwaggerConfig.java

--- src/main/java/fr/istv/Bugtracking/route/BugRoute.java
+++ src/main/java/fr/istv/Bugtracking/route/BugRoute.java
@@ -1,10 +1,14 @@
 package fr.istv.Bugtracking.route;
 
+import fr.istv.Bugtracking.entity.Bug;
 import fr.istv.Bugtracking.repository.BugRepository;
 import fr.istv.Bugtracking.service.XBugService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BugRoute<BugService> {
@@ -15,7 +19,26 @@ public class BugRoute<BugService> {
     @Autowired
     XBugService bugService;
 
-    @GetMapping("?")
-    public?
+    @GetMapping("bug/{ID}") {
+        public Bug getBug (@PathVariable("ID") int id){
+            return bugRepository.findById(id);
+        }
+    }
 
+    @GetMapping("bug")
+    public List<Bug> getAllSBugs() {
+        return bugRepository.findAll();
+    }
+
+    @GetMapping("bug/{priority}"){
+        public List<Bug> getByPriority(@PathVariable("priority") String priority){
+            return bugRepository.findByPriority(priority);
+        }
+    }
+
+    @GetMapping("bug/{priority}"){
+        public List<Bug> getByState(@PathVariable("state") String state){
+            return bugRepository.findByState(state);
+        }
+    }
 }
